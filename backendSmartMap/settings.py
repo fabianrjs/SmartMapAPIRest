0a783c61f067072aae9cef99d6196ab5cf680c63
rojax <fabian_rojas-10@hotmail.com>
2022-10-29 23:16:11 -0500

    properties creado

--- backendSmartMap/settings.py
+++ backendSmartMap/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from . import properties
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h&2r9h&zb-7i&a7mu^6n53476t)56w(z8wax2pfhyc!zc()tvy'
+SECRET_KEY = properties.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
