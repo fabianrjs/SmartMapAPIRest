7b61c4d2acab35b57ee3fe953d9960fbee799648
rojax <fabian_rojas-10@hotmail.com>
2022-10-29 23:51:20 -0500

    Edificio: atributo imagen cambiado a nulleable

--- apiRest/models.py
+++ apiRest/models.py
@@ -5,4 +5,4 @@ class Edificio(models.Model):
     nombre = models.CharField(max_length = 100)
     apodo = models.CharField(max_length = 100, null = True)
     descripcion = models.CharField(max_length = 500)
-    imagen = models.CharField(max_length = 500, default = 'https://cdn.forbes.co/2020/09/Javeriana-1280x720-1.jpg')
+    imagen = models.CharField(max_length = 500, null = True)
