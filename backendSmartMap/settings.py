d0c325f0a4efbd8be31259fef5f13882c7602b61
rojax <fabian_rojas-10@hotmail.com>
2022-11-05 19:13:10 -0500

    properties eliminado

--- backendSmartMap/settings.py
+++ backendSmartMap/settings.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from . import properties
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,16 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if 'SECRET_KEY' in os.environ:
-    SECRET_KEY = os.environ['SECRET_KEY']
-else:
-    SECRET_KEY = properties.SECRET_KEY
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -51,9 +46,6 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-#CORS_ORIGIN_WHITELIST = (
-#    'http://localhost:8081',
-#)
 
 ROOT_URLCONF = 'backendSmartMap.urls'
 
@@ -79,17 +71,9 @@ WSGI_APPLICATION = 'backendSmartMap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DB_NAME = ''
-if 'DB_NAME' in os.environ:
-    DB_NAME = os.environ['DB_NAME']
-else:
-    DB_NAME = properties.DB_NAME
+DB_NAME = os.environ['DB_NAME']
 
-HOST_DB = ''
-if 'HOST_DB' in os.environ:
-    HOST_DB = os.environ['HOST_DB']
-else:
-    HOST_DB = properties.HOST_DB
+HOST_DB = os.environ['HOST_DB']
 
 
 DATABASES = {
