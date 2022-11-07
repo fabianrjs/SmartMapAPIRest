f111e8c44b6dd5be65906b24a3eff7d1750caf12
rojax <fabian_rojas-10@hotmail.com>
2022-11-06 21:08:38 -0500

    piso añadido

--- apiRest/models.py
+++ apiRest/models.py
@@ -12,12 +12,13 @@ class Espacio(models.Model):
         LABORATORIO = 'LAB','Laboratorio'
         SALA_ESTUDIO = 'SE','Sala de estudio'
         SALON = 'SA','Salon'
-        SALA_COMPUTO = 'SC','Sala de computo' 
+        SALA_COMPUTO = 'SC','Sala de computo'
 
     nombre = models.CharField(max_length = 100, default = "", null = True)
     sePuedeComer = models.BooleanField(default = False, null = True)
     tipoEspacio = models.CharField(choices=TIPO_ESPACIO.choices, null = True)
-    prestamoEquipos =  models.BooleanField(default = False, null = True)
+    prestamoEquipos = models.BooleanField(default = False, null = True)
+    piso = models.CharField(max_length = 3)
 
     class Meta:
         abstract = True
